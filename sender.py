982dc81074e09277a0636980f6990daab052f0a1
jrrpm <rodrigopintomendez@hotmail.com>
2017-02-13 19:32:21 -0400

    Update sender.py

--- sender.py
+++ sender.py
@@ -8,20 +8,13 @@ from yowsup.env import YowsupEnv
 logging.basicConfig(level=logging.DEBUG)
 
 def credential():
-    return "59176329787","zTh+17TK6rGpK//zrj1FKQMFDjw="
+    return "",""
 
-def Answer(risp):
-    try:
-        stack=YowsupSendStack(credential(), ["59176048595", risp], True)
-        stack.start()
-    except:
-        pass
-    return
 
 def startSendClient():
         try:
             #self.printInfoText()
-            stack=YowsupSendStack(("59176329787","zTh+17TK6rGpK//zrj1FKQMFDjw="), [("59176048595", "hola mundoo")])
+            stack=YowsupSendStack(("",""), [("", "hola mundoo")])
             stack.start()
         except KeyboardInterrupt:
             print("\nYowsdown")
