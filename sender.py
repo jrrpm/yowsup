4ebb618e9e85aebb000e6d998430e2b22d5ddd33
Rodrigo Pinto <rodrigopintomendez@hotmail.com>
2017-02-14 16:51:14 -0400

    sender

--- sender.py
+++ sender.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys, argparse, yowsup, logging
 from yowsup.demos.sendclient import YowsupSendStack
 from yowsup.env import YowsupEnv
@@ -8,18 +10,17 @@ from yowsup.env import YowsupEnv
 logging.basicConfig(level=logging.DEBUG)
 
 def credential():
-    return "",""
+    return "59178503175","ytp8UDps0CRhUD1yOoIDu1avGKA="
 
 
-def startSendClient():
+def send(phone, msg):
         try:
             #self.printInfoText()
-            stack=YowsupSendStack(("",""), [("", "hola mundoo")])
+            stack=YowsupSendStack(credential(), [(phone, msg)])
             stack.start()
         except KeyboardInterrupt:
             print("\nYowsdown")
             #sys.exit(0)
 
 
-startSendClient()
-#Answer("Updating...")
+send("59176048595","Hola, rodrigo, como estas? XD")
