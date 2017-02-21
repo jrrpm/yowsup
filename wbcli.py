17256f9bffad8b652e7c9ee74071396734a5bde8
Rodrigo Pinto <rodrigopintomendez@hotmail.com>
2017-02-21 18:09:23 -0400

    mongodb impl

--- wbcli.py
+++ wbcli.py
@@ -2,13 +2,20 @@
 
 import sys, argparse, yowsup, logging
 from yowsup.wabot import YowsupBotStack
+from pymongo import MongoClient
 
 #todo log level at config file
 logging.basicConfig(level=logging.INFO)
 
+global oPuntopagos
+
+client = MongoClient("mongodb://localhost:27017") #todo db auth+config file
+db = client.wabots
+oPuntopagos = db.puntosPago.find_one()
+
 #todo db credential
 def credential():
-    return "",""
+    return oPuntopagos[u'phone'],oPuntopagos[u'password']
 
 stack = YowsupBotStack(credential())
 stack.start()
